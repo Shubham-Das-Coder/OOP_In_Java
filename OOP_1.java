8511d0ca34eac95cdcfc2cf1ea03b4fb2dfec9f8
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2023-03-31 01:28:16 +0530

    Add files via upload

---
+++ OOP_1.java
@@ -0,0 +1,9 @@
+public class OOP_1 {
+    public static void main(String args[]) {
+        int[] numbers = new int[5];
+        String[] names = new String[5];
+        int[] rno = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+    }
+}
